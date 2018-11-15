d515ecd5f3db28188e458cf23bb851b5f07e7fe7
Irina Popa <irina.oana.popa@gmail.com>
2018-11-15 18:11:19 +0000

    added doses mofos

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,37 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    # @cocktail = Cocktail.find(params[:cocktail_id]) - we need @cocktail in the form
+    @dose = Dose.new
+  end
+
+
+  def create
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to @dose.cocktail
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ require 'open-uri'
 
 puts "Destroy ingredients"
 Ingredient.destroy_all
-
-puts "Destroy Cocktails"
-Cocktail.destroy_all
+puts "Adding ingredients"
 
 puts "Create ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
 
-ingredients["drinks"].each do |ingredient|
+ingredients["drinks"].take(10).each do |ingredient|
   i = Ingredient.create(name: ingredient["strIngredient1"])
   puts "create #{i.name}"
 end
 
 
+puts "Destroy Cocktails"
+Cocktail.destroy_all
 puts "Adding Cocktails"
 Cocktail.create(name: "NegronIRI")
 Cocktail.create(name: "MargaIRIta")
@@ -25,8 +25,14 @@ Cocktail.create(name: "CosmopolitanIRI")
 Cocktail.create(name: "CaipIRInha")
 
 
-# p "Adding Ingredients"
-# Ingredient.create(name: "whiskey")
-# Ingredient.create(name: "fresh lemon juice")
-# Ingredient.create(name: "syrup")
-# Ingredient.create(name: "crushed ice")
+
+puts "Destroying Doses"
+Dose.destroy_all
+p "Adding Doses"
+
+Dose.create(description: "1 part", ingredient_id: Ingredient.all.sample, cocktail_id: Cocktail.all.sample)
+Dose.create(description: "a drop of", ingredient_id: Ingredient.all.sample, cocktail_id: Cocktail.all.sample)
+Dose.create(description: "2 parts", ingredient_id: Ingredient.all.sample, cocktail_id: Cocktail.all.sample)
+Dose.create(description: "a splash of", ingredient_id: Ingredient.all.sample, cocktail_id: Cocktail.all.sample)
+
+p "Seeds finished!"
