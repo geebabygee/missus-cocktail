50388ae75c552a36b2fe79c1ae7e685573dfb677
Irina Popa <irina.oana.popa@gmail.com>
2018-11-15 19:46:26 +0000

    photos up

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,8 +2,6 @@ class Cocktail < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     has_many :doses, dependent: :destroy
     has_many :ingredients, through: :doses
+    mount_uploader :photo, PhotoUploader
 end
     # has_many :reviews, dependent: :destroy
-    # mount_uploader :photo, PhotoUploader
-
-
