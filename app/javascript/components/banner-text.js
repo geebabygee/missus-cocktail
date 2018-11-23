e14ba5102ac57b3af4eda7ebd39e7700de5e19b2
Irina Popa <irina.oana.popa@gmail.com>
2018-11-23 12:48:42 +0000

    added typed.js

---
+++ app/javascript/components/banner-text.js
@@ -0,0 +1,16 @@
+
+import Typed from 'typed.js';
+
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ['Cocktails for wild nights'],
+    typeSpeed: 200,
+    // loop: false
+  });
+}
+
+export { loadDynamicBannerText };
+
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,8 @@
 
 console.log('Hello World from Webpacker')
 import 'bootstrap';
+
+import { loadDynamicBannerText } from 'components/banner-text';
+
+
+loadDynamicBannerText();
