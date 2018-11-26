383c09fbdf0e5010e737cb7e0ed461699312b8ae
Irina Popa <irina.oana.popa@gmail.com>
2018-11-26 20:59:04 +0000

    if photo nil

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -11,8 +11,3 @@ class Dose < ApplicationRecord
 #delete the ingredient itself....for the routes! - See shallow nesting in slides
 
 
-
-  # cocktail cannot be blank (FAILED - 8)
-  # ingredient cannot be blank (FAILED - 9)
-  # is unique for a given cocktail/ingredient couple (FAILED - 10)
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ puts "Create ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
 
-ingredients["drinks"].take(10).each do |ingredient|
+ingredients["drinks"].each do |ingredient|
   i = Ingredient.create(name: ingredient["strIngredient1"])
   puts "create #{i.name}"
 end
