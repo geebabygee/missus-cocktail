0109546a0410844702d3421e21c8b1957e7f4453
Irina Popa <irina.oana.popa@gmail.com>
2018-11-27 12:35:34 +0000

    no doses yet

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,6 @@ class Cocktail < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     has_many :doses, dependent: :destroy
     has_many :ingredients, through: :doses
-    mount_uploader :photo, PhotoUploader
     has_many :reviews, dependent: :destroy
+    mount_uploader :photo, PhotoUploader
 end
-    # has_many :reviews, dependent: :destroy
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,8 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses
-  # has_many :cocktails, through: :doses
+  has_many :cocktails, through: :doses
 end
 
 
 
-  # should not be able to destroy self if has dose children (FAILED - 15)
